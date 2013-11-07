895ed0d77c2e0ba9d0f0c9039edff5e04eab7b86
Don Lamont <don.lamont@e-pict.net>
2013-11-07 18:19:19 +0000

    Removing test code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcMusicStore.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "I like cake! I'm wondering if this will be picked up by Git for Windows";
+            ViewBag.Message = "I like cake!";
 
             return View();
         }
